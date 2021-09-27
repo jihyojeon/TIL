7c8bf9921254e18dc6a219616bedfe134bed0496
Jihyo Jeon <jihyo@gm.gist.ac.kr>
2021-09-27 17:04:24 +0100

    Add Ruby and C

---
+++ Ruby/tutorial.rb
@@ -0,0 +1,36 @@
+# Printing
+puts 'Goodbye, World!'
+puts 'newline test'
+p 'Goodbye, World!'
+p 'newline test'
+print 'Goodbye, World!'
+print 'new line test'
+# Goodbye, World!
+# newline test
+# "Goodbye, World!"
+# "newline test"
+# Goodbye, World!new line test
+
+# Variables
+myString = 'I am programming!'
+myBoolean = true
+myNumber = 5
+puts myString
+puts myBoolean
+puts myNumber
+# I am a programming!
+# true
+# 5
+
+# Math
+testOne = 6 * 5 #30
+testTwo = 36 % 6 #0 나머지
+testThree = 12 - 5 #7
+
+# Strings
+"Hi!".length #is 3
+"Hi!".reverse #is !iH
+"Hi!".upcase #is HI!
+"Hi!".downcase #is hi!
+"Hi!".downcase.reverse #is !ih
+"Happy Birthday!".include?("Happy")
