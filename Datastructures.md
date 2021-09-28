ab4d967761e41702a2c43c82a87a2e7df0f1aadd
Jihyo Jeon <jihyo@gm.gist.ac.kr>
2021-09-28 10:28:02 +0100

    Add datastructures

---
+++ Datastructures.md
@@ -0,0 +1,20 @@
+### Data Structures
+
+## Stack
+It’s like a stack of paper.
+It offers “push” and “pop” methods.
+In programming, function calls form a stack.
+It’s a “LIFO” collection (i.e. “last in, first out”).
+
+## Queue
+It’s like a queue at a restaurant.
+It offers “enqueue” and “dequeue” methods.
+In computers, CPU operations form a queue.
+It’s a “FIFO” collection (i.e. “first in, first out”).
+
+## Linked list
+It’s a linear collection of data elements, called nodes, each pointing to the next node by means of a pointer.
+In double linked lists, each node also has a pointer to the previous one.
+Insertion and deletion of nodes is not expensive.
+There is no need to define an initial size.
+It’s a dynamic data structure.
--- Ruby/tutorial.rb
+++ Ruby/tutorial.rb
@@ -33,4 +33,78 @@ testThree = 12 - 5 #7
 "Hi!".upcase #is HI!
 "Hi!".downcase #is hi!
 "Hi!".downcase.reverse #is !ih
-"Happy Birthday!".include?("Happy")
+"Happy Birthday!".include?("Happy") //true
+
+# Arrays
+myArray = []  # an empty array
+myOtherArray = [1, 2, 3]  # an array with three elements
+myOtherArray[3] = 4
+
+# Hashes and Symbols
+myHash={
+  "Key" => "value",
+  "Key2" => "value2"
+}
+puts myHash["Key"] # puts value
+# Or
+myHash=Hash.new()
+myHash["Key"]="value"
+myHash["Key2"]="value2"
+puts myHash[:Key] # puts "value"
+# Or
+myHash={
+  Key: "value",
+  Key2: "value2",
+}
+puts myHash[:Key] # puts "value"
+
+# Conditional Statements
+# if condition
+#   something to be done
+# end
+booleanOne = true
+randomCode = "Hi!"
+if booleanOne
+  puts "I will be printed!"
+elsif randomCode.length>=1
+  puts "Even though the above code is true, I won't be executed because the earlier if statement was true!"
+else
+  puts "I won't be printed because the if statement was executed!"
+end
+
+# Unless, Until, and While
+# unless condition
+#   #thing to be done if the condition is false
+# end
+# while condition
+#   #something to do
+# end
+# until condition
+#   #something to do
+# end
+#Make your loop below!
+i = 0
+while i < 7
+    puts "I'm looping!"
+    i = i + 1
+end
+# I'm looping!
+# I'm looping!
+# I'm looping!
+# I'm looping!
+# I'm looping!
+# I'm looping!
+# I'm looping!
+
+
+# Methods
+# def methodname
+#   # method code here
+# end
+#define the method here
+def square(number)
+  puts number**2
+end
+#don't forget to call the method with the argument
+square(2)
+
