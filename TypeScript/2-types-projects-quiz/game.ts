f35eedf10287efdf0a1e234745a6d6a884323b69
Jihyo Jeon <jihyo@gm.gist.ac.kr>
2021-11-06 17:22:24 +0000

    Add TS toy problems

---
+++ TypeScript/2-types-projects-quiz/game.ts
@@ -0,0 +1,40 @@
+type coordinate = {
+  x: number;
+  y: number;
+};
+
+let position: coordinate = {
+  x: 0,
+  y: 0,
+};
+
+type command = 'up' | 'down' | 'left' | 'right';
+
+function move(direction: command) {
+  switch (direction) {
+    case 'up':
+      position.y += 1;
+      break;
+    case 'down':
+      position.y -= 1;
+      break;
+    case 'left':
+      position.x -= 1;
+      break;
+    case 'right':
+      position.x += 1;
+      break;
+    default:
+      throw new Error('Error');
+  }
+}
+
+console.log(position); // { x: 0, y: 0}
+move('up');
+console.log(position); // { x: 0, y: 1}
+move('down');
+console.log(position); // { x: 0, y: 0}
+move('left');
+console.log(position); // { x: -1, y: 0}
+move('right');
+console.log(position); // { x: 0, y: 0}
---
+++ TypeScript/2-types-projects-quiz/loading.ts
@@ -0,0 +1,42 @@
+{
+  /**
+   * Print Loading State
+   */
+  type LoadingState = {
+    state: 'loading';
+  };
+
+  type SuccessState = {
+    state: 'success';
+    response: {
+      body: string;
+    };
+  };
+
+  type FailState = {
+    state: 'fail';
+    reason: string;
+  };
+
+  type ResourceLoadState = LoadingState | SuccessState | FailState;
+
+  function printLoginState(obj: ResourceLoadState) {
+    switch (obj.state) {
+      case 'loading':
+        console.log('👀 loading...');
+        break;
+      case 'success':
+        console.log(`😃 ${obj.response.body}`);
+        break;
+      case 'fail':
+        console.log(`😱 ${obj.reason}`);
+        break;
+      default:
+        throw new Error('Unknown state');
+    }
+  }
+
+  printLoginState({ state: 'loading' }); // 👀 loading...
+  printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
+  printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
+}
